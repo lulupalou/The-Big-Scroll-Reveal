35b878956fba438fba77e28d200d19f263befc11
lulupalou <palou.a.craig@gmail.com>
2026-02-21 03:34:21 +0000

    Refine

--- utilities.js
+++ utilities.js
@@ -1,6 +1,6 @@
 ScrollReveal().reveal('.pink', { delay: 200 });
 ScrollReveal().reveal('.mix', { distance: '100px' });
-ScrollReveal().reveal('.purple', { scale: 1});
+ScrollReveal().reveal('.purple', { scale: 2});
 ScrollReveal().reveal('.orange', {
     rotate: {
         x: 20,
