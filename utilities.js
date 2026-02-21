bd175abe1a930c47971de1e9cadf0fac12492baf
lulupalou <palou.a.craig@gmail.com>
2026-02-21 03:27:47 +0000

    update

--- utilities.js
+++ utilities.js
@@ -1,5 +1,10 @@
 ScrollReveal().reveal('.pink', { delay: 200 });
-ScrollReveal().reveal('.mix', { delay: 300 });
-ScrollReveal().reveal('.purple', { delay: 400 });
-ScrollReveal().reveal('.orange', { delay: 500 });
-ScrollReveal().reveal('.butterfly', { delay: 600 });
+ScrollReveal().reveal('.mix', { distance: '100px' });
+ScrollReveal().reveal('.purple', { scale: 1});
+ScrollReveal().reveal('.orange', {
+    rotate: {
+        x: 20,
+        z: 20
+    }
+});
+ScrollReveal().reveal('.butterfly', { delay: 1000 });
